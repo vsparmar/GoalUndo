8788d194eb8905fc62486f2c9541439842eef7ed
kbuffardi <kbuffardi@csuchico.edu>
2016-05-12 20:04:44 -0700

    Add GoalUndoTest and make automation

---
+++ GoalUndoTest.cpp
@@ -0,0 +1,21 @@
+/**
+ * Unit Tests for ConnectX class
+**/
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include "GoalUndo.h"
+ 
+class GoalUndoTest : public ::testing::Test
+{
+	protected:
+		GoalUndoTest(){}
+		virtual ~GoalUndoTest(){}
+		virtual void SetUp(){}
+		virtual void TearDown(){}
+};
+
+TEST(GoalUndoTest, sanityCheck)
+{
+	ASSERT_TRUE(true);
+}
